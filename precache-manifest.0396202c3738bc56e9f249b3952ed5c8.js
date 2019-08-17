61c0c4a544a08af97f850e4005715a277ed38891
Daniel Frye <daniel_frye@autoonemedia.com>
2019-08-17 00:37:15 -0500

    Updates

---
+++ precache-manifest.0396202c3738bc56e9f249b3952ed5c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a06df5652a777cbfa997d973c38e12d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d81437eb0789e1a98447",
+    "url": "/monsters-rolodex/static/css/main.6c600b91.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "d81437eb0789e1a98447",
+    "url": "/monsters-rolodex/static/js/main.4fa94dc9.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
